a42bde43deb17b348ff2caf774b2236de6e565b1
vv <yuwei@gwu.edu>
2020-11-08 20:52:53 -0500

    Updates

---
+++ precache-manifest.70f15979276e81a4d4ecb3ceaad7cb59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c051829779eeb1f0d70f",
+    "url": "./static/css/main.3578b86b.chunk.css"
+  },
+  {
+    "revision": "c051829779eeb1f0d70f",
+    "url": "./static/js/main.f668c47c.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "7b8bdc765504f34cece4",
+    "url": "./static/css/2.ae4e41dd.chunk.css"
+  },
+  {
+    "revision": "7b8bdc765504f34cece4",
+    "url": "./static/js/2.436cc341.chunk.js"
+  },
+  {
+    "revision": "80c4c16749145a2b4d74ac9810eaaacb",
+    "url": "./index.html"
+  }
+];
